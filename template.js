52557a7f01468d11a9bef0787a9c68c6f6f89fb8
FlyingTurtle69 <58875407+FlyingTurtle69@users.noreply.github.com>
2021-12-19 21:19:33 +1000

    example script

---
+++ template.js
@@ -0,0 +1,21 @@
+// Modification from https://bitburner.readthedocs.io/en/latest/netscript/netscriptjs.html
+
+/** @param {import(".").NS } ns */
+export async function main(ns) {
+    const target = ns.args[0];
+    const moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+    const securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+
+    if (ns.fileExists("BruteSSH.exe", "home")) ns.brutessh(target);
+    ns.nuke(target);
+
+    while (true) {
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            await ns.grow(target);
+        } else {
+            await ns.hack(target);
+        }
+    }
+}
